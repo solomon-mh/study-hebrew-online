08991f1859683d253882fffb56d123302e0b15b6
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-10-06 21:37:08 +0300

    api -fix

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const lesson = require("./LessonData");
-const path = require("path"); 
+const path = require("path");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -12,7 +12,6 @@ const port = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
-
 
 // API Endpoints
 
@@ -68,11 +67,11 @@ app.use(express.static(buildPath));
 
 // Handle all other routes by serving index.html
 app.get("*", (req, res) => {
-  res.sendFile(path.join(buildPath, "index.html"), function (err) {
-    if (err) {
-      res.status(500).send(err);
-    }
-  });
+	res.sendFile(path.join(buildPath, "index.html"), function (err) {
+		if (err) {
+			res.status(500).send(err);
+		}
+	});
 });
 
 // Start the server
