c901d7fd0020405605ae8136e963af338a12ed8e
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-10-07 07:32:32 +0300

    route-fix

--- frontend/src/components/lessons/ExercisesSection.jsx
+++ frontend/src/components/lessons/ExercisesSection.jsx
@@ -1,26 +1,13 @@
 // src/components/ExercisesSection.js
 
-import axios from "axios";
-import React, { useEffect, useState } from "react";
-
+import React, { useState } from "react";
 import ExerciseComprehension from "./ExerciseComprehension";
 import ExerciseMatching from "./ExerciseMatching";
 import ExerciseFillInTheBlanks from "./ExerciseFillInTheBlanks";
 
-function ExercisesSection() {
-	const [exercises, setExercises] = useState([]);
+function ExercisesSection({ exercises }) {
 	const [userAnswers, setUserAnswers] = useState({});
 	const [score, setScore] = useState(null);
-
-	// Fetch exercises from backend
-	useEffect(() => {
-		axios
-			.get("http://localhost:5000/api/lesson")
-			.then((response) => {
-				setExercises(response.data.exercises);
-			})
-			.catch((error) => console.error(error));
-	}, []);
 
 	const handleAnswerChange = (exerciseIndex, questionIndex, answer) => {
 		setUserAnswers((prevAnswers) => {
@@ -34,12 +21,41 @@ function ExercisesSection() {
 	};
 
 	const handleSubmit = () => {
-		axios
-			.post("http://localhost:5000/api/validate", { userAnswers })
-			.then((response) => {
-				setScore(response.data.score);
-			})
-			.catch((error) => console.error(error));
+		let totalQuestions = 0;
+		let correctAnswers = 0;
+
+		exercises.forEach((exercise, exerciseIndex) => {
+			if (exercise.type === "comprehension") {
+				exercise.questions.forEach((question, questionIndex) => {
+					totalQuestions += 1;
+					const userAnswer = userAnswers[exerciseIndex]?.[questionIndex];
+					if (userAnswer === question.correctAnswer) {
+						correctAnswers += 1;
+					}
+				});
+			} else if (exercise.type === "matching") {
+				exercise.pairs.forEach((pair, index) => {
+					totalQuestions += 1;
+					const userAnswer = userAnswers[exerciseIndex]?.[index];
+					if (userAnswer === pair.english) {
+						correctAnswers += 1;
+					}
+				});
+			} else if (exercise.type === "fillInTheBlanks") {
+				exercise.sentences.forEach((sentenceObj, questionIndex) => {
+					totalQuestions += Object.keys(sentenceObj.correctAnswers).length;
+					const userAnswer = userAnswers[exerciseIndex]?.[questionIndex];
+					Object.keys(sentenceObj.correctAnswers).forEach((blank) => {
+						if (userAnswer?.[blank] === sentenceObj.correctAnswers[blank]) {
+							correctAnswers += 1;
+						}
+					});
+				});
+			}
+		});
+
+		const calculatedScore = Math.round((correctAnswers / totalQuestions) * 100);
+		setScore(calculatedScore);
 	};
 
 	return (
